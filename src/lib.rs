5428cb63d5f53b3107607842eeed0433d55a47a4
Mathias Pius <contact@pius.io>
2023-01-10 13:29:49 +0100

    De-emphasize Fallible structures and variants in comments. Add crate-level flags to ensure documnetation and code structure. Add missing documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,27 @@
 //! [`Fallible`] is an [`Option`] with inverted [`Try`]-semantics.
 //!
 //! What this means is that using the `?` operator on a [`Fallible<E>`] will exit early
-//! if an error `E` is contained within, or instead act as a no-op, if the value is [`Fallible::Ok`].
+//! if an error `E` is contained within, or instead act as a no-op, if the value is [`Intact`].
+//!
+//! It fills the gap left by the [`Result`] and [`Option`] types:
+//!
+//! | `Result<T`  |     `, E>`    |
+//! |-------------|---------------|
+//! | `Option<T>` | `Fallible<E>` |
+//!
+//!
 //!
 //! ```
-//! # use fallible::Fallible;
+//! # use fallible::Fallible::{self, Fail, Intact};
 //! # fn test_chained_failures() {
 //! // Check many numbers, returning early if a tested
 //! // number is equal to zero.
 //! fn check_many_numbers() -> Fallible<&'static str> {
 //!     let fails_if_number_is_zero = |n: u32| {
 //!         if n == 0 {
-//!             Fallible::Err("number is zero")
+//!             Fail("number is zero")
 //!         } else {
-//!             Fallible::Ok
+//!             Intact
 //!         }
 //!     };
 //!
@@ -22,17 +30,18 @@
 //!
 //!     // Following lines are never reached
 //!     fails_if_number_is_zero(10)?;
-//!     Fallible::Ok
+//!     
+//!     Intact
 //! }
 //!
-//! assert_eq!(check_many_numbers(), Fallible::Err("number is zero"));
+//! assert_eq!(check_many_numbers(), Fallible::Fail("number is zero"));
 //! # }
 //! ```
 //! # Purpose
 //! [`Fallible`] is similar in usage to `Result<(), E>`, but without introducing a
-//! unit value `Ok(())` to cover the [`Ok`] case.
-//! 
-//! For example, the following function could be better expressed using fallible, since
+//! unit value `Ok(())` to cover the [`Intact`] case.
+//!
+//! For example, the following function could be better expressed using [`Fallible`], since
 //! the happy path does not produce a value:
 //! ```
 //! fn validate_number(x: u32) -> Result<(), &'static str> {
@@ -44,43 +53,69 @@
 //! }
 //! ```
 //! Using [`Fallible`]:
-//! 
+//!
 //! ```
-//! # use fallible::Fallible;
+//! # use fallible::Fallible::{self, Fail, Intact};
 //! fn validate_number(x: u32) -> Fallible<&'static str> {
 //!     match x {
-//!         0 ..= 9 => Fallible::Err("number is too small"),
-//!         10..=30 => Fallible::Ok,
-//!         31..    => Fallible::Err("number is too large")
+//!         0 ..= 9 => Fail("number is too small"),
+//!         10..=30 => Intact,
+//!         31..    => Fail("number is too large")
 //!     }
 //! }
 //! ```
 //! # Compatibility
-//! 
+//!
 //! [`Fallible`] contains utility functions for mapping to and from [`Result`] and [`Option`],
 //! as well as [`FromResidual`] implementations for automatically performing these conversions
 //! when used with the `?` operator.
 //! ```
-//! # use fallible::Fallible;
-//! fn fails_if_true(fail: bool) -> Fallible<&'static str> {
-//!     if fail {
-//!         Fallible::Err("this function always fails!")
+//! # use fallible::Fallible::{self, Fail, Intact};
+//! fn fails_if_true(should_fail: bool) -> Fallible<&'static str> {
+//!     if should_fail {
+//!         Fail("Darn it!")
 //!     } else {
-//!         Fallible::Ok
+//!         Intact
 //!     }
 //! }
-//! 
+//!
 //! fn try_producing_value() -> Result<u32, &'static str> {
 //!     fails_if_true(false)?;
 //!     fails_if_true(true)?;
-//! 
+//!
 //!     Ok(10)
 //! }
 //! ```
-//! 
-//! 
+//!
+//!
 
 #![no_std]
+#![deny(
+    bad_style,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    unconditional_recursion,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true,
+    missing_debug_implementations,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results
+)]
+#![forbid(unsafe_code)]
 #![feature(try_trait_v2)]
 #![feature(const_trait_impl)]
 #![feature(const_mut_refs)]
@@ -91,12 +126,18 @@ use core::marker::Destruct;
 use core::mem;
 use core::ops::{ControlFlow, Deref, DerefMut, FromResidual, Try};
 
+/// Describes the outcome of an operation which does not produce a value
+/// if the operation succeeds. Similar in usage to [`Result<(), E>`].
 #[must_use]
 #[derive(Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 pub enum Fallible<E> {
-    Ok,
-    Err(E),
+    /// No error was produced.
+    Intact,
+    /// An error was produced.
+    Fail(E),
 }
+
+use Fallible::{Fail, Intact};
 
 impl<E> Fallible<E> {
     /// Converts from `Fallible<E>` (or `&Fallible<E>`) to `Fallible<&E::Target>`.
@@ -109,8 +150,8 @@ impl<E> Fallible<E> {
         E: ~const Deref,
     {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(e) => Fallible::Err(e.deref()),
+            Intact => Intact,
+            Fail(e) => Fail(e.deref()),
         }
     }
 
@@ -124,8 +165,8 @@ impl<E> Fallible<E> {
         E: ~const DerefMut,
     {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(e) => Fallible::Err(e.deref_mut()),
+            Intact => Intact,
+            Fail(e) => Fail(e.deref_mut()),
         }
     }
 
@@ -133,8 +174,8 @@ impl<E> Fallible<E> {
     #[inline]
     pub const fn as_mut(&mut self) -> Fallible<&mut E> {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(ref mut e) => Fallible::Err(e),
+            Intact => Intact,
+            Fail(ref mut e) => Fail(e),
         }
     }
 
@@ -142,43 +183,43 @@ impl<E> Fallible<E> {
     #[inline]
     pub const fn as_ref(&self) -> Fallible<&E> {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(ref e) => Fallible::Err(e),
+            Intact => Intact,
+            Fail(ref e) => Fail(e),
         }
     }
 
-    /// Returns true if the value is a [`Fallible::Ok`], otherwise false.
+    /// Returns true if the value is a `Intact`, otherwise false.
     #[inline]
-    pub const fn is_ok(&self) -> bool {
-        matches!(self, Fallible::Ok)
+    pub const fn is_intact(&self) -> bool {
+        matches!(self, Intact)
     }
 
-    /// Returns true if the value is a [`Fallible::Err`], otherwise false.
+    /// Returns true if the value is a `Fail`, otherwise false.
     #[inline]
-    pub const fn is_err(&self) -> bool {
-        matches!(self, Fallible::Err(_))
+    pub const fn is_fail(&self) -> bool {
+        matches!(self, Fail(_))
     }
 
     /// Unwrap the contained error or panics if no error has occurred.
     #[inline]
-    pub fn unwrap_err(self) {
+    pub fn unwrap_fail(self) {
         match self {
-            Fallible::Ok => panic!("called `Fallible::unwrap_err()` on a `Fallible::Ok` value"),
-            Fallible::Err(_) => (),
+            Intact => panic!("called `Fallible::unwrap_fail()` on a `Fallible::Intact` value"),
+            Fail(_) => (),
         }
     }
 
-    /// Returns `true` if the fallible is a [`Fallible::Err`] value containing an error
+    /// Returns `true` if the fallible is a `Fail` value containing an error
     /// equivalent to `f`
     #[inline]
     pub const fn contains<F: ~const PartialEq<E>>(&self, f: &F) -> bool {
         match self {
-            Fallible::Ok => false,
-            Fallible::Err(e) => f.eq(e),
+            Intact => false,
+            Fail(e) => f.eq(e),
         }
     }
 
-    /// Maps a [`Fallible<E>`] to [`Fallible<F>`] by applying a function
+    /// Maps a `Fallible<E>` to `Fallible<F>` by applying a function
     /// to the contained error.
     #[inline]
     pub const fn map<F, O>(self, op: O) -> Fallible<F>
@@ -188,34 +229,37 @@ impl<E> Fallible<E> {
         E: ~const Destruct,
     {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(e) => Fallible::Err(op(e)),
+            Intact => Intact,
+            Fail(e) => Fail(op(e)),
         }
     }
 
-    /// Transforms the [`Fallible<E>`] into a [`Result<(), E>`], where [`Fallible::Err(e)`]
-    /// becomes [`Err(e)`] and [`Fallible::Ok`] becomes [`Ok(())`]
+    /// Transforms the `Fallible<E>` into a [`Result<(), E>`], where `Fail(e)`
+    /// becomes `Err(e)` and `Intact` becomes `Ok(())`
     #[inline]
     pub const fn result(self) -> Result<(), E>
     where
         E: ~const Destruct,
     {
         match self {
-            Fallible::Ok => Ok(()),
-            Fallible::Err(e) => Err(e),
+            Intact => Ok(()),
+            Fail(e) => Err(e),
         }
     }
 
+    /// Borrows the `Fallible<E>` as an [`Option<E>`], yielding none
+    /// if no error occurred.
     #[inline]
     pub const fn err(&self) -> Option<&E> {
         match self {
-            Fallible::Ok => None,
-            Fallible::Err(err) => Some(err),
+            Intact => None,
+            Fail(err) => Some(err),
         }
     }
 
-    /// Constructs a [`Result<T, E>`] from self, [`Fallible::Err(e)`]
-    /// becomes [`Err(e)`] and [`Fallible::Ok`] becomes [`Ok(value)`]
+    /// Constructs a [`Result<T, E>`] from self.
+    ///
+    /// `Fail(e)` becomes `Err(e)` and `Intact` becomes `Ok(value)`
     #[inline]
     pub const fn err_or<T>(self, value: T) -> Result<T, E>
     where
@@ -223,19 +267,22 @@ impl<E> Fallible<E> {
         T: ~const Destruct,
     {
         match self {
-            Fallible::Ok => Ok(value),
-            Fallible::Err(e) => Err(e),
+            Intact => Ok(value),
+            Fail(e) => Err(e),
         }
     }
 
+    /// Replaces the contained error (if any) with None,
+    /// and returns an [`Option<E>`] with the contained error,
+    /// if the outcome was `Fail`.
     #[inline]
     pub const fn take(&mut self) -> Option<E>
     where
         E: ~const Destruct,
     {
-        match mem::replace(self, Fallible::Ok) {
-            Fallible::Ok => None,
-            Fallible::Err(e) => Some(e),
+        match mem::replace(self, Intact) {
+            Intact => None,
+            Fail(e) => Some(e),
         }
     }
 }
@@ -244,14 +291,14 @@ impl<E> Fallible<&E>
 where
     E: ~const Clone,
 {
-    /// Maps an `Fallible<&E>` to an `Fallible<E>` by cloning the contents of the
+    /// Maps a `Fallible<&E>` to a [`Fallible<E>`] by cloning the contents of the
     /// error.
     #[inline]
     #[must_use = "`self` will be dropped if the result is not used"]
     pub const fn cloned(self) -> Fallible<E> {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(e) => Fallible::Err(e.clone()),
+            Intact => Intact,
+            Fail(e) => Fail(e.clone()),
         }
     }
 
@@ -264,8 +311,8 @@ where
         E: Copy,
     {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(&e) => Fallible::Err(e),
+            Intact => Intact,
+            Fail(&e) => Fail(e),
         }
     }
 }
@@ -280,8 +327,8 @@ where
     #[must_use = "`self` will be dropped if the result is not used"]
     pub const fn cloned(self) -> Fallible<E> {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(e) => Fallible::Err(e.clone()),
+            Intact => Intact,
+            Fail(e) => Fail(e.clone()),
         }
     }
 
@@ -294,8 +341,8 @@ where
         E: Copy,
     {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(&mut e) => Fallible::Err(e),
+            Intact => Intact,
+            Fail(&mut e) => Fail(e),
         }
     }
 }
@@ -305,32 +352,32 @@ impl<E> Fallible<E>
 where
     E: Debug,
 {
-    /// Returns a unit value if the [`Fallible`] is not an [`Fallible::Err`].
+    /// Returns a unit value if the `Fallible` is not `Fail`.
     ///
     /// # Panics
-    /// Panics if the value is a [`Fallible::Err`], with a panic message including
-    /// the content of the [`Fallible::Err`].
+    /// Panics if the value is a `Fail`, with a panic message including
+    /// the content of the `Fail`.
     #[inline]
     pub fn unwrap(self) {
         match self {
-            Fallible::Ok => (),
-            Fallible::Err(e) => {
-                panic!("called `Fallible::unwrap()` on a `Fallible::Err` value: {e:?}")
+            Intact => (),
+            Fail(e) => {
+                panic!("called `Fallible::unwrap()` on a `Fallible::Fail` value: {e:?}")
             }
         }
     }
 }
 
 impl<E> Fallible<Fallible<E>> {
-    /// Flattens a [`Fallible<Fallible<E>>`] into a [`Fallible<E>`]
+    /// Flattens a `Fallible<Fallible<E>>` into a `Fallible<E>`
     #[inline]
     pub const fn flatten(self) -> Fallible<E>
     where
         E: ~const Destruct,
     {
         match self {
-            Fallible::Ok => Fallible::Ok,
-            Fallible::Err(inner) => inner,
+            Intact => Intact,
+            Fail(inner) => inner,
         }
     }
 }
@@ -338,7 +385,7 @@ impl<E> Fallible<Fallible<E>> {
 impl<E> const From<E> for Fallible<E> {
     #[inline]
     fn from(value: E) -> Self {
-        Fallible::Err(value)
+        Fail(value)
     }
 }
 
@@ -350,8 +397,8 @@ where
     #[inline]
     fn from(value: Result<T, E>) -> Self {
         match value {
-            Ok(_) => Fallible::Ok,
-            Err(e) => Fallible::Err(e),
+            Ok(_) => Intact,
+            Err(e) => Fail(e),
         }
     }
 }
@@ -373,7 +420,7 @@ impl<'a, E> const From<&'a mut Fallible<E>> for Fallible<&'a mut E> {
 impl<E> const Default for Fallible<E> {
     #[inline]
     fn default() -> Self {
-        Fallible::Ok
+        Intact
     }
 }
 
@@ -384,15 +431,15 @@ where
     #[inline]
     fn clone(&self) -> Self {
         match self {
-            Fallible::Err(x) => Fallible::Err(x.clone()),
-            Fallible::Ok => Fallible::Ok,
+            Fail(x) => Fail(x.clone()),
+            Intact => Intact,
         }
     }
 
     #[inline]
     fn clone_from(&mut self, source: &Self) {
         match (self, source) {
-            (Fallible::Err(to), Fallible::Err(from)) => to.clone_from(from),
+            (Fail(to), Fail(from)) => to.clone_from(from),
             (to, from) => *to = from.clone(),
         }
     }
@@ -404,14 +451,14 @@ impl<E> Try for Fallible<E> {
 
     #[inline]
     fn from_output(_: Self::Output) -> Self {
-        Fallible::Ok
+        Intact
     }
 
     #[inline]
     fn branch(self) -> ControlFlow<Self::Residual, Self::Output> {
         match self {
-            Fallible::Ok => ControlFlow::Continue(()),
-            Fallible::Err(e) => ControlFlow::Break(Fallible::Err(e)),
+            Intact => ControlFlow::Continue(()),
+            Fail(e) => ControlFlow::Break(Fail(e)),
         }
     }
 }
@@ -427,8 +474,8 @@ impl<T, E> FromResidual<Fallible<E>> for Result<T, E> {
     #[inline]
     fn from_residual(residual: Fallible<E>) -> Self {
         match residual {
-            Fallible::Ok => unreachable!(),
-            Fallible::Err(e) => Err(e),
+            Intact => unreachable!(),
+            Fail(e) => Err(e),
         }
     }
 }
@@ -437,8 +484,8 @@ impl<E> FromResidual<Result<(), E>> for Fallible<E> {
     #[inline]
     fn from_residual(residual: Result<(), E>) -> Self {
         match residual {
-            Ok(()) => Fallible::Ok,
-            Err(e) => Fallible::Err(e),
+            Ok(()) => Intact,
+            Err(e) => Fail(e),
         }
     }
 }
@@ -447,8 +494,8 @@ impl<E> FromResidual<Result<Infallible, E>> for Fallible<E> {
     #[inline]
     fn from_residual(residual: Result<Infallible, E>) -> Self {
         match residual {
-            Ok(_) => Fallible::Ok,
-            Err(e) => Fallible::Err(e),
+            Ok(_) => Intact,
+            Err(e) => Fail(e),
         }
     }
 }
